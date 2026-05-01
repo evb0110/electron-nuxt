dc6143223eeb492f56ce78de5a94dada98556505
Eugene Barsky <34789976+evb0110@users.noreply.github.com>
2026-05-01 18:28:48 +0300

    Share serialized save and zoom log helpers

---
+++ app/modules/pdf-viewer-runtime/composables/pdfViewerZoomTypes.ts
@@ -0,0 +1,8 @@
+export interface IViewerStateForLog {
+    scrollTop: number;
+    scrollLeft: number;
+    clientWidth: number;
+    clientHeight: number;
+    scrollWidth: number;
+    scrollHeight: number;
+}
