9d0b68d6cd999707c2e5ea07ae6e18326d5e9e6e
Eugene Barsky <34789976+evb0110@users.noreply.github.com>
2026-05-12 19:40:44 +0300

    Fix tab title tooltip hover target

--- app/modules/workspace-shell/components/layout/TabBar.vue
+++ app/modules/workspace-shell/components/layout/TabBar.vue
@@ -1,48 +1,49 @@
 <template>
     <div ref="tabBarRef" class="tab-bar">
         <div class="tab-list" role="tablist">
-            <AppTooltip
+            <div
                 v-for="(tab, index) in tabs"
                 :key="tab.id"
-                :text="resolveTabTitle(tab)"
-                :delay-duration="800"
+                :data-tab-id="tab.id"
+                role="tab"
+                class="tab"
+                :class="{
+                    'is-active': tab.id === activeTabId,
+                    'is-dragging': isDragging && dragIndex === index,
+                }"
+                :aria-label="resolveTabTitle(tab)"
+                :aria-selected="tab.id === activeTabId"
+                :tabindex="tab.id === activeTabId ? 0 : -1"
+                @click="handleTabClick(tab.id)"
+                @auxclick.prevent="handleAuxClick($event, tab.id)"
+                @keydown="handleTabKeydown($event, tab.id)"
+                @pointerdown="onPointerDown($event, index)"
+                @contextmenu.prevent.stop="openTabContextMenu($event, tab.id)"
             >
-                <div
-                    :data-tab-id="tab.id"
-                    role="tab"
-                    class="tab"
-                    :class="{
-                        'is-active': tab.id === activeTabId,
-                        'is-dragging': isDragging && dragIndex === index,
-                    }"
-                    :aria-label="resolveTabTitle(tab)"
-                    :aria-selected="tab.id === activeTabId"
-                    :tabindex="tab.id === activeTabId ? 0 : -1"
-                    @click="handleTabClick(tab.id)"
-                    @auxclick.prevent="handleAuxClick($event, tab.id)"
-                    @keydown="handleTabKeydown($event, tab.id)"
-                    @pointerdown="onPointerDown($event, index)"
-                    @contextmenu.prevent.stop="openTabContextMenu($event, tab.id)"
+                <AppTooltip
+                    :text="resolveTabTitle(tab)"
+                    :delay-duration="800"
+                    usefulness="overflow"
                 >
                     <span class="tab-label">{{ tab.fileName ?? t('tabs.newTab') }}</span>
-                    <span
-                        v-if="tab.isDirty"
-                        class="tab-dirty-dot"
-                        :aria-label="t('tabs.unsavedChanges')"
-                    />
-                    <button
-                        type="button"
-                        class="tab-close"
-                        :class="{ 'is-visible': tab.id === activeTabId }"
-                        :aria-label="t('tabs.closeTab')"
-                        :disabled="!canCloseTabs"
-                        @pointerdown.stop
-                        @click.stop="requestClose(tab.id)"
-                    >
-                        <Icon name="ph:x" size="14" />
-                    </button>
-                </div>
-            </AppTooltip>
+                </AppTooltip>
+                <span
+                    v-if="tab.isDirty"
+                    class="tab-dirty-dot"
+                    :aria-label="t('tabs.unsavedChanges')"
+                />
+                <button
+                    type="button"
+                    class="tab-close"
+                    :class="{ 'is-visible': tab.id === activeTabId }"
+                    :aria-label="t('tabs.closeTab')"
+                    :disabled="!canCloseTabs"
+                    @pointerdown.stop
+                    @click.stop="requestClose(tab.id)"
+                >
+                    <Icon name="ph:x" size="14" />
+                </button>
+            </div>
             <button
                 type="button"
                 class="tab-new"
