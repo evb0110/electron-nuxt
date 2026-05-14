690f5032aab534014d6a048f6ea57ff986474767
Eugene Barsky <34789976+evb0110@users.noreply.github.com>
2026-05-14 03:09:31 +0300

    Persist browser install hint dismissal

--- app/utils/browserRuntimePersistence.ts
+++ app/utils/browserRuntimePersistence.ts
@@ -1,2 +1,3 @@
 export const BROWSER_RECENT_FILES_STORAGE_KEY = 'evb-viewer:browser:recentFiles';
 export const BROWSER_INSTALL_HINT_COOKIE_KEY = 'evb_viewer_web_install_hint_dismissed';
+export const BROWSER_INSTALL_HINT_STORAGE_KEY = 'evb-viewer:browser:installHintDismissed';
